5855a69fa7b0a2b76e3aaa462d1e65fbe1ba18ae
thejustinlombardi <thejustinlombardi@gmail.com>
2022-01-15 18:10:56 -0500

    Add envelope styling

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -3,8 +3,8 @@ import "./Welcome.css";
 function Welcome() {
 	return (
 		<div className="welcome-container">
-			<div className="welcome-one"></div>
 			<div className="welcome-two"></div>
+			<div className="welcome-one"></div>
 			<div className="welcome-three"></div>
 			<div className="welcome-four"></div>
 			<div className="welcome-five"></div>
