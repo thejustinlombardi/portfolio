9f421e8a341016efc245bff3227c2ee96d8c6512
thejustinlombardi <thejustinlombardi@gmail.com>
2022-01-15 16:39:20 -0500

    Set up framework

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,15 @@
+import "./Welcome.css";
+
+function Welcome() {
+	return (
+		<div className="welcome-container">
+			<div className="welcome-one"></div>
+			<div className="welcome-two"></div>
+			<div className="welcome-three"></div>
+			<div className="welcome-four"></div>
+			<div className="welcome-five"></div>
+		</div>
+	);
+}
+
+export default Welcome;
