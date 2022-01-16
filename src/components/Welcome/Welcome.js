a48fcf1a0a86ec86984f3a82ba2f207c382cc603
thejustinlombardi <thejustinlombardi@gmail.com>
2022-01-15 22:40:22 -0500

    Add style to home page

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -3,10 +3,19 @@ import "./Welcome.css";
 function Welcome() {
 	return (
 		<div className="welcome-container">
-			<div className="welcome-two"></div>
-			<div className="welcome-one"></div>
-			<div className="welcome-three"></div>
-			<div className="welcome-four"></div>
+			<div className="welcome-one">
+				<h2>ORIGINS OF</h2>
+				<h1>JUSTIN C LOMBARDI</h1>
+			</div>
+			<div className="welcome-two">
+				<img src="https://i.imgur.com/sTG3UGN.jpg" alt="justin-lombardi" />
+			</div>
+			<div className="welcome-three">
+				<img src="https://i.imgur.com/sTG3UGN.jpg" alt="justin-lombardi" />
+			</div>
+			<div className="welcome-four">
+				<img src="https://i.imgur.com/sTG3UGN.jpg" alt="justin-lombardi" />
+			</div>
 			<div className="welcome-five"></div>
 		</div>
 	);
