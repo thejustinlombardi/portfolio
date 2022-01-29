b90dc6eb7dc89beee3bc9e1051def40770e4abf0
thejustinlombardi <thejustinlombardi@gmail.com>
2022-01-29 01:19:24 -0500

    Complete draft of portfolio mobile and desktop

--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -1,14 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function ProjectList({ projects }) {
 	return (
-		<div className="hero-card">
+		<div className="project-card">
 			{projects.map((project) => {
 				return (
 					<Link
 						to={`/home/projects/${project.id}`}
 						className="issues-div about-div">
-						<img className="card-img" src={project.image} alt={project.title} />
+						<img
+							className="project-card-img"
+							src={project.image}
+							alt={project.title}
+						/>
 						<h2>{project.title}</h2>
 					</Link>
 				);
