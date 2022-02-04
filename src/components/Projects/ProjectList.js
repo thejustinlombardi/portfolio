8f1572b93febceea2d74acaaf47546db5844a7ee
thejustinlombardi <thejustinlombardi@gmail.com>
2022-02-03 19:51:50 -0500

    Connect backend to frontend and seed project data

--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -1,13 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function ProjectList({ projects }) {
 	return (
 		<div className="project-card">
-			{projects.map((project) => {
+			{projects.map((project, index) => {
 				return (
 					<Link
 						to={`/home/projects/${project.id}`}
-						className="issues-div about-div">
+						className="issues-div about-div"
+						key={`${project.title}-${index}`}>
 						<img
 							className="project-card-img"
 							src={project.image}
